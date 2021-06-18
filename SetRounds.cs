027903c8f1723870afeb6b8972500dfaec3be713
Ascyst <85650794+Ascyst@users.noreply.github.com>
2021-06-18 09:23:51 -0700

    Bumping missed version no.

--- SetRounds.cs
+++ SetRounds.cs
@@ -6,65 +6,81 @@ using UnboundLib;
 using UnboundLib.GameModes;
 using UnityEngine;
 using System.Collections;
+using UnboundLib.Networking;
 
 namespace SetRoundsPlugin
 {
     [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
-    [BepInPlugin(ModId, ModName, "1.0.0")]
+    [BepInPlugin(ModId, ModName, "1.1.0")]
     [BepInProcess("Rounds.exe")]
     public class SetRounds : BaseUnityPlugin
     {
         private struct NetworkEventType
         {
-            public const string SyncRounds = (ModId + "_Sync");
+            public const string SyncRounds = (ModId + "_SyncRounds");
+            public const string SyncPoints = (ModId + "_SyncPoints");
         }
         private const string ModId = "com.ascyst.rounds.setrounds";
 
         private const string ModName = "Set Rounds";
 
         public static string setRounds = "5";
+        public static string setPoints = "2";
 
         public static int syncRounds = 5;
-
-
+        public static int syncPoints = 2;
+        private IEnumerator SetRound(IGameModeHandler gm)
+        {
+            gm.ChangeSetting("roundsToWinGame", syncRounds);
+            gm.ChangeSetting("pointsToWinRound", syncPoints);
+            yield break;
+        }
         private void Awake()
         {
             new Harmony(ModId).PatchAll();
-            NetworkingManager.RegisterEvent(NetworkEventType.SyncRounds, sync => setRounds = (string)sync[0]);
-            GameModeManager.AddHook(GameModeHooks.HookInitEnd, (gm) =>
-            {
-                gm.ChangeSetting("roundsToWinGame", syncRounds);
-            });
+            GameModeManager.AddHook(GameModeHooks.HookInitStart, SetRound);
         }
+
+
 
         private void Start()
         {
-
             Unbound.RegisterGUI("Set Rounds per Game", new Action(this.DrawGUI));
             Unbound.RegisterHandshake(ModId, new Action(this.OnHandShakeCompleted));
         }
 
         private void DrawGUI()
         {
-            string flag = GUILayout.TextField(setRounds, 3);
-            if (flag != setRounds && PhotonNetwork.IsMasterClient)
+            GUILayout.Label("Rounds per Game");
+            var rounds = GUILayout.TextField(setRounds, 3);
+
+            GUILayout.Label("Points per Round");
+            var points = GUILayout.TextField(setPoints, 1);
+
+            if (PhotonNetwork.OfflineMode || PhotonNetwork.CurrentRoom == null)
             {
-                NetworkingManager.RaiseEvent(NetworkEventType.SyncRounds, flag);
-            }
-            setRounds = flag;
-            if (PhotonNetwork.OfflineMode)
-            {
+                setRounds = rounds;
+                setPoints = points;
                 int.TryParse(setRounds, out syncRounds);
+                int.TryParse(setPoints, out syncPoints);
             }
         }
 
         private void OnHandShakeCompleted()
         {
+            int.TryParse(setRounds, out syncRounds);
+            int.TryParse(setPoints, out syncPoints);
             if (PhotonNetwork.IsMasterClient)
             {
-                NetworkingManager.RaiseEvent(NetworkEventType.SyncRounds, setRounds);
+                NetworkingManager.RPC_Others(typeof(SetRounds), nameof(UpdateValues), syncRounds, syncPoints);
             }
-            int.TryParse(setRounds, out syncRounds);
+        }
+
+        [UnboundRPC]
+        private static void UpdateValues(int roundsToWin, int pointsToWin)
+        {
+            syncRounds = roundsToWin;
+            syncPoints = pointsToWin;
         }
     }
 }
