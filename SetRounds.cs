0fe4e022c87af319fabf7a08018ea41108ff2f59
pdcook <qphysicscook@gmail.com>
2021-10-27 17:49:55 -0400

    fixed desync, config saves

--- SetRounds.cs
+++ SetRounds.cs
@@ -9,25 +9,26 @@ using UnityEngine;
 using System.Collections;
 using UnboundLib.Networking;
 using TMPro;
+using BepInEx.Configuration;
 
 namespace SetRoundsPlugin
 {
     [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
-    [BepInPlugin(ModId, ModName, "1.2.0")]
+    [BepInPlugin(ModId, ModName, "1.2.1")]
     [BepInProcess("Rounds.exe")]
     public class SetRounds : BaseUnityPlugin
     {
-        private struct NetworkEventType
-        {
-            public static int SyncRounds = setRounds;
-            public static int SyncPoints = setPoints;
-        }
+        private static ConfigEntry<int> SetRoundsConfig;
+        private static ConfigEntry<int> SetPointsConfig;
+
         private const string ModId = "com.ascyst.rounds.setrounds";
 
         private const string ModName = "Set Rounds";
+        private const string CompatibilityModName = "SetRounds";
 
-        public static int setRounds = 5;
-        public static int setPoints = 2;
+        public static int setRounds;
+        public static int setPoints;
+
         public IEnumerator SetRound(IGameModeHandler gm)
         {
             gm.ChangeSetting("roundsToWinGame", setRounds);
@@ -35,27 +36,38 @@ namespace SetRoundsPlugin
             yield break;
         }
 
+        private void Awake()
+        {
+            SetRoundsConfig = Config.Bind(CompatibilityModName, "Number of rounds required to win the game", 5);
+            SetPointsConfig = Config.Bind(CompatibilityModName, "Number of points required to win a round", 2);
+        }
         private void Start()
         {
+            // call settings not as not to orphan them
+            setRounds = SetRoundsConfig.Value;
+            setPoints = SetPointsConfig.Value;
+
             Unbound.RegisterHandshake(ModId, OnHandShakeCompleted);
             Unbound.RegisterMenu("Set Rounds", () => { }, NewGUI, null, false);
-            GameModeManager.AddHook(GameModeHooks.HookInitEnd, SetRound);
+            GameModeManager.AddHook(GameModeHooks.HookGameStart, SetRound);
         }
 
         void RoundSliderAction(float val)
         {
+            SetRoundsConfig.Value = (int)val;
             setRounds = (int)val;
         }
 
         void PointSliderAction(float val)
         {
+            SetPointsConfig.Value = (int)val;
             setPoints = (int)val;
         }
         private void NewGUI(GameObject menu)
         {
             MenuHandler.CreateText("Set Rounds", menu, out TextMeshProUGUI _, 60);
-            MenuHandler.CreateSlider("Rounds per Match", menu, 50, 1f, 30f, setRounds, RoundSliderAction, out UnityEngine.UI.Slider roundSlider, true);
-            MenuHandler.CreateSlider("Points per Round", menu, 50, 1f, 30f, setPoints, PointSliderAction, out UnityEngine.UI.Slider pointSlider, true);
+            MenuHandler.CreateSlider("Rounds per Match", menu, 50, 1f, 30f, SetRoundsConfig.Value, RoundSliderAction, out UnityEngine.UI.Slider roundSlider, true);
+            MenuHandler.CreateSlider("Points per Round", menu, 50, 1f, 30f, SetPointsConfig.Value, PointSliderAction, out UnityEngine.UI.Slider pointSlider, true);
         }
 
         private void OnHandShakeCompleted()
